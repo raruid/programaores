40d80a718c98df66a1f0ebb190883689eb168518
Pablocassal <pablo.cassal2000@gmail.com>
2019-02-21 14:16:32 +0100

    Raul1

--- OrdenaStruckt.cpp
+++ OrdenaStruckt.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+int imprimealumnos(const int alumno[], int util_alum);
+
 int main(){
   const int VALUE = 10;
   persona alumno[VALUE];
@@ -14,4 +16,12 @@ int main(){
   imprimealumnos(alumno, util_alumnos);
 
   cout << "Ya se ha acabao el programa chavalin, vete a tu casa" << endl;
+}
+
+int imprimealumnos(const int &alumno[], int util_alum){
+
+  cout << "Te he ordenado los alumnos de mas tonto a mas listo: "
+  for (int i = 0; i < util_alum; i++){
+    cout << alumno[i].nombre << endl;
+  }
 }
